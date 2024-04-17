341f23ecffaa98c9fb93b762f65bdec03122d9ef
vigyanAbhishek <abhishek@shuniyavigyan.com>
2024-04-17 13:15:17 +0530

    deleted

--- server/env.js
+++
@@ -1,8 +0,0 @@
-// # PORT = 5000
-// # RAZORPAY_KEY_ID = rzp_test_3tO53sYAumFdEC
-// # RAZORPAY_SECRET = 6RMYdGQajg8JbNZ3icf6Vsfn
-
-module.exports={
-    Email: "shownothing925@gmail.com",
-    pass: 'alrk wadf evrp ouak'
-}
--- server/routes/transRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const transactionController = require('../controllers/transController');
-
-// Route for inserting a transaction
-router.post('/', transactionController.insertTransaction);
-
-module.exports = router;
